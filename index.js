f62aeb1b7e7252e043dfa03d11c67381555549de
juliana <juliana@slash.co>
2022-09-23 13:52:54 +0700

    modify

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function pushZeroToTheEnd(nums) {
-  const zeros = nums.filter((n) => n === 0);
-  const nonZeros = nums.filter((n) => n !== 0);
-
-  const result = [...nonZeros, ...zeros];
-
-  return result;
+  return nums.sort(function (a, b) {
+    if (a === 0 && b !== 0) {
+      return 1;
+    } else if (b === 0 && a !== 0) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  //const zeros = nums.filter((n) => n === 0); //O(n)
+  //const nonZeros = nums.filter((n) => n !== 0); //O(n)
+  //const result = [...nonZeros, ...zeros];
+  //return result;
 }
 
 const result1 = pushZeroToTheEnd([0, 1, 0, 3, 5]); // [1, 3, 5, 0, 0]
